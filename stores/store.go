50cdb3885734a21ab0b4980e3736fa085c55618c
MarceJua <mjuarez2017ig@gmail.com>
2025-03-19 16:40:45 -0600

    Se agrega comando mkdir

--- stores/store.go
+++ stores/store.go
@@ -85,3 +85,38 @@ func GetMountedPartitionRep(id string) (*structures.MBR, *structures.SuperBlock,
 
 	return &mbr, &sb, path, nil
 }
+
+// GetMountedPartitionSuperblock obtiene el SuperBlock de la partición montada con el id especificado
+func GetMountedPartitionSuperblock(id string) (*structures.SuperBlock, *structures.Partition, string, error) {
+	// Obtener el path de la partición montada
+	path := MountedPartitions[id]
+	if path == "" {
+		return nil, nil, "", errors.New("la partición no está montada")
+	}
+
+	// Crear una instancia de MBR
+	var mbr structures.MBR
+
+	// Deserializar la estructura MBR desde un archivo binario
+	err := mbr.Deserialize(path)
+	if err != nil {
+		return nil, nil, "", err
+	}
+
+	// Buscar la partición con el id especificado
+	partition, err := mbr.GetPartitionByID(id)
+	if partition == nil {
+		return nil, nil, "", err
+	}
+
+	// Crear una instancia de SuperBlock
+	var sb structures.SuperBlock
+
+	// Deserializar la estructura SuperBlock desde un archivo binario
+	err = sb.Deserialize(path, int64(partition.Part_start))
+	if err != nil {
+		return nil, nil, "", err
+	}
+
+	return &sb, partition, path, nil
+}
